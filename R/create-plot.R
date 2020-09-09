406fe7e062f6f3ba2952f83b8637f8c1dee08cce
trsketch <theo+sketch@jumpingrivers.com>
2020-09-09 16:08:47 +0100

    Changing point colour / adding new script

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour = "red")
+  geom_point(colour = "blue")
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
