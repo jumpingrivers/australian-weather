fdec4edcb8809e88b7cce1047243e86ad5ed3e48
trsketch <theo+sketch@jumpingrivers.com>
2020-09-10 14:58:38 +0100

    Change to create plot

--- R/create-plot.R
+++ R/create-plot.R
@@ -9,14 +9,15 @@ df = read_csv("data/weatherAUS.csv")
 # Create a scatter plot of Humidity9am and Temp9am
 # a location
 
-city = "Brisbane"
+city = "Sydney"
 
 scatter_plot = 
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point(colour = "pink") + 
-  labs(caption = "This data is copyright of the Austr blah")
+  labs(caption = "This data is copyright of the Austr blah") + 
+  theme_minimal()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
