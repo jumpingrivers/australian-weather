69d818cb4ea60600fb86d13b59a3408dd004352a
trsketch <theo+sketch@jumpingrivers.com>
2020-09-10 13:48:22 +0100

    Change points from blue to pink

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour = "blue")
+  geom_point(colour = "pink")
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
