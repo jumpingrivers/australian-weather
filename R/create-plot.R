77adfe4e85fd4825bac820b0c2b7f4ec4a9a36da
amy bowes <amy.bowes@crick.ac.uk>
2021-02-23 16:26:05 +0000

    change plot

--- R/create-plot.R
+++ R/create-plot.R
@@ -11,11 +11,13 @@ df = read_csv("data/weatherAUS.csv")
 
 city = "Brisbane"
 
-scatter_plot = 
-df %>%
-  filter(Location == city) %>%
-  ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour = "red")
+scatter_plot =
+  df %>%
+  filter(Location == "Brisbane") %>%
+  ggplot(aes(x = Temp9am, y = Humidity9am)) +
+  geom_point(colour = "red") +
+  labs(title = "Humidity and Temperatur for Brisbane") +
+  theme_minimal()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
