fe5b114c4b9d5349750f102b4fed79bb10794192
tdavy-private <afrodude1337@gmail.com>
2021-02-23 15:56:28 +0000

    Faceted scatter plot by Wind Direction; Script tidying

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,10 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am), ) + 
-  geom_point(color="red") + ggtitle(paste0("Humidity vs. Temperature in ",city)) + theme_minimal()
+  geom_point(color="red") + 
+  ggtitle(paste0("Humidity vs. Temperature in ",city)) + 
+  theme_minimal() +
+  facet_wrap(~WindDir9am)
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
