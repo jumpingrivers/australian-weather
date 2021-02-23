75a663a64c9e6feae850b2ceed559d3184a40462
tdavy-private <afrodude1337@gmail.com>
2021-02-23 15:07:29 +0000

    Added title to Humidity vs. Temperature plot

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am), ) + 
-  geom_point(color="red")
+  geom_point(color="red") + ggtitle(paste0("Humidity vs. Temperature in ",city))
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
