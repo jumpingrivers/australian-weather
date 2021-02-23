4e04e57efb73c4120542680787faf33314fce3c5
Eleanor Childs <eleanorchilds1@gmail.com>
2021-02-23 16:43:40 +0000

    Facet by wind direction

--- R/create-plot.R
+++ R/create-plot.R
@@ -17,7 +17,8 @@ scatter_plot =
   ggplot(aes(x = Temp9am, y = Humidity9am)) +
   geom_point(colour = "red") +
   labs(title = "Humidity and Temperatur for Brisbane") +
-  theme_minimal()
+  theme_minimal() +
+  facet_wrap(~WindDir9am)
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
