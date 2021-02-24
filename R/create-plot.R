e582e07e099fe72762afe77cfac07673a408f8da
amy bowes <amy.bowes@crick.ac.uk>
2021-02-24 14:27:04 +0000

    Create timeseries

--- R/create-plot.R
+++ R/create-plot.R
@@ -21,3 +21,12 @@ scatter_plot =
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
+
+# New plot
+time_series =
+  df %>%
+  filter(Location == city) %>%
+  ggplot(aes(x = Date, y = Temp9am)) +
+  geom_line()
+ggsave(filename = "graphics/timeseries-rainfall.png",
+       plot = scatter_plot)
