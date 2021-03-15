e8c06a499867124fc79a02280e0b8e3d873a878d
Anne Alarilla <anne.alarilla@health.org.uk>
2021-03-15 15:50:56 +0000

    added a minimal theme in the plot

--- R/create-plot.R
+++ R/create-plot.R
@@ -16,7 +16,8 @@ df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point(colour="red")+
-  labs(title="Humidity by Temp in Sydney")
+  labs(title="Humidity by Temp in Sydney")+
+  theme_minimal()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
