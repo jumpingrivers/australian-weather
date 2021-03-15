726dc11bbe34b1fb26e84f01f76f458c25fcd229
edinkasia <kasia.banas@gmail.com>
2021-03-15 15:00:26 +0000

    added title to the plot

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,8 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour = "red")
+  geom_point(colour = "red") +
+  ggtitle("Temperature and humidity in Brisbane")
 
 
 # Save the plot in graphics/
