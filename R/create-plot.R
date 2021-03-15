a33deb6b2237deecceae003c5b3872b2bd9f2944
Hannah Sawyer <hannah-sawyer@hotmail.co.uk>
2021-03-15 15:50:59 +0000

    added theme_minimal() to plot

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am, colour = "red")) + 
-  geom_point()
+  geom_point() + theme_minimal()
 
 scatter_plot
 
