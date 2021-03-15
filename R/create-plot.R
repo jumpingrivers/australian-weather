f0f79aabb9e3cc60a2df1aa3ab268bd5a38a69f8
Anne Alarilla <anne.alarilla@health.org.uk>
2021-03-15 15:06:16 +0000

    Adding a title

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,8 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour="red")
+  geom_point(colour="red")+
+  labs(title="Humidity by Temp in Sydney")
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
