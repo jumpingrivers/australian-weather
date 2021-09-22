4bf43b633972ea3239acaf6f5b9750dfdc8b0397
jessedwards2411 <jessicaedwards240@gmail.com>
2021-09-22 11:06:01 +0100

    Changed points in plot to red and added a view of it.

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,10 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point()
+  geom_point(colour = "red")
+
+scatter_plot
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
+
