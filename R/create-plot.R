b3a452d582033a3cd538d0a5af55c45a8404ac57
jessedwards2411 <jessicaedwards240@gmail.com>
2021-09-23 11:37:30 +0100

    Add time series plot

--- R/create-plot.R
+++ R/create-plot.R
@@ -22,6 +22,16 @@ df %>%
 
 scatter_plot
 
+
+time_series =
+  df %>%
+  filter(Location == city) %>%
+  ggplot(aes(x = Date, y = Temp9am)) +
+  geom_line()
+ggsave(filename = "graphics/timeseries-rainfall.png",
+       plot = time_series)
+
+
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
 
