63a7ed883d75f4bfc100d3e1fd11193887cd9a0c
John McIntyre <john@jumpingrivers.com>
2021-10-21 11:20:40 +0100

    Add avg rainfall bar chart

--- R/create-plot.R
+++ R/create-plot.R
@@ -21,9 +21,17 @@ df %>%
 bar_plot = df %>%
   ggplot(aes(x = WindGustDir)) +
   geom_bar(fill = "blue")
+
+bar_chart =
+  df %>%
+  group_by(Location) %>%
+  summarise(average_rainfall = mean(Rainfall, na.rm = TRUE)) %>%
+  ggplot(aes(y = Location, x = average_rainfall)) +
+  geom_col()
   
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
 ggsave(filename = "graphics/wind_gust_dir_bar.png", plot = bar_plot)
+ggsave(filename = "graphics/barchart-avg-rainfall.png", plot = bar_chart)
 
 
