0ba1887d176f0adaccd7c2783b7e06c01d1eced7
John McIntyre <john@jumpingrivers.com>
2021-10-21 10:07:56 +0100

    Add minimal theme to plot

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,8 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour = "red")
+  geom_point(colour = "red") +
+  theme_minimal()
 
 bar_plot = df %>%
   ggplot(aes(x = WindGustDir)) +
