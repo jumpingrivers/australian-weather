e42f0ab5e0dccced476d3f5e7829a5737473cc63
LConradt <larissa.conradt@nhs.net>
2021-10-21 10:53:00 +0100

    with time series

--- R/create-plot.R
+++ R/create-plot.R
@@ -24,3 +24,17 @@ bar_plot =
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
 ggsave(filename = "graphics/BarWind.png", plot = bar_plot)
+
+
+# time series
+
+time_series =
+  df %>%
+  filter(Location == city) %>%
+  ggplot(aes(x = Date, y = Temp9am)) +
+  geom_line()
+ggsave(filename = "graphics/timeseries-rainfall.png",
+       plot = time_series)
+
+
+
