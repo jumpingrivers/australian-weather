aeafd9876890ba564d245d720ebe9683d1a4c007
Adnan Shroufi <adnanshroufi@gmail.com>
2021-10-21 12:09:49 +0100

    Add line graph

--- R/create-plot.R
+++ R/create-plot.R
@@ -27,9 +27,13 @@ time_series =
   df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Date, y = Temp9am)) +
-  geom_line()
+  geom_line()+
+  theme_classic()
+
+
+
 ggsave(filename = "graphics/timeseries-rainfall.png",
-       plot = time_series)
+       plot = time_series)+
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
