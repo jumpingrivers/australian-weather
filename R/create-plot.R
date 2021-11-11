60344a5a020aa2148fb2207b7b717e72395698f9
Charlie Steer <charles.steer@nhs.net>
2021-11-11 10:56:47 +0000

    add title based on city name and re-ggsave plot

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,8 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour = "red")
+  geom_point(colour = "red") +
+  ggtitle(str_c(city," humidity plot"))
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
