234e3e39bbca80973ffad5b11375a0c1cb6f1c61
Tom Jemmett 2 <andrewhood@nhs.net>
2022-06-22 16:32:56 +0100

    added geom_smooth line to plot

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,8 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(color = "red", size = 1.2)
+  geom_point(color = "red", size = 1.2) +
+  geom_smooth()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
