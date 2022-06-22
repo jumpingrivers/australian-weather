61d7b34badd4e5b294f9afdf9a1b59e91aa2c835
Tom Jemmett 2 <andrewhood@nhs.net>
2022-06-22 16:52:38 +0100

    add dynamic title

--- R/create-plot.R
+++ R/create-plot.R
@@ -10,13 +10,15 @@ df = read_csv("data/weatherAUS.csv")
 # a location
 
 city = "Townsville"
+title = paste("Scatterplot of temparature and humidity in",city,"2007 to 2017")
 
 scatter_plot = 
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point(color = "red", size = 1.2) +
-  geom_smooth()
+  geom_smooth() +
+  ggtitle(lab = title)
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
