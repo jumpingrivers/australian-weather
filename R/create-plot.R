da3853af3c3e0c2bde2005278f5055040e5c7c35
Tom Jemmett 2 <andrewhood@nhs.net>
2022-06-23 15:59:54 +0100

    Create violin plot

--- R/create-plot.R
+++ R/create-plot.R
@@ -10,7 +10,7 @@ df = read_csv("data/weatherAUS.csv")
 # a location
 
 city = "WaggaWagga"
-title = paste("Scatterplot of temparature and humidity in",city,"2007 to 2017")
+title = paste("Scatterplot of temparature and humidity in",city,": 2007 to 2017")
 
 scatter_plot = 
 df %>%
@@ -23,3 +23,23 @@ df %>%
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
+
+# Create a violin plot of Maximum temperature
+title2 = paste("Violinplot of Max Temperature in",city,": 2007 to 2017")
+
+violin_plot = 
+  df %>%
+  filter(Location == city) %>%
+  ggplot(aes(x=Location, y=MaxTemp)) +
+  geom_violin(fill = "red", alpha=0.6, draw_quantiles=c(.25, .50, .75)) +
+  geom_jitter(color="blue", size=0.4, alpha=0.3)+
+  ggtitle(lab = title2) +
+  theme_minimal() +
+  theme(
+    legend.position="none",
+    plot.title = element_text(size=11)
+  )
+
+ggsave(filename = "graphics/max-temperature.png", plot = violin_plot)
+
+  
