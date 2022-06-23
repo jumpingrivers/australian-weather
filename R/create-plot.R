ce25dba417d090efa4f5ec0b7906b65004ef6c71
StaceyTraining <staceyhcroft@gmail.com>
2022-06-23 13:56:14 +0100

    Changed to Brisbane and minimal theme

--- R/create-plot.R
+++ R/create-plot.R
@@ -9,13 +9,14 @@ df = read_csv("data/weatherAUS.csv")
 # Create a scatter plot of Humidity9am and Temp9am
 # a location
 
-city = "Sydney"
+city = "Brisbane"
 
 scatter_plot = 
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) +
   geom_point(colour = "red")+ 
+  theme_minimal()
   labs(title = paste0("Scatter plot of Humidity9am and Temp9am in ",city),
                                    subtitle = "Plot for training")
 
