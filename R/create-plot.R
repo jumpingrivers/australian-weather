2fb5fe50cd4876abc44b2becdfe61ed7acee1968
Tom Jemmett 2 <andrewhood@nhs.net>
2022-06-23 14:33:13 +0100

    Change to WaggaWagga and plot theme

--- R/create-plot.R
+++ R/create-plot.R
@@ -9,7 +9,7 @@ df = read_csv("data/weatherAUS.csv")
 # Create a scatter plot of Humidity9am and Temp9am
 # a location
 
-city = "Townsville"
+city = "WaggaWagga"
 title = paste("Scatterplot of temparature and humidity in",city,"2007 to 2017")
 
 scatter_plot = 
@@ -18,7 +18,8 @@ df %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point(color = "red", size = 1.2) +
   geom_smooth() +
-  ggtitle(lab = title)
+  ggtitle(lab = title) +
+  theme_minimal()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
