57f65c856c44db72654814fe8d277a01a9ae2999
markfergusondfc <mark.ferguson@communities-ni.gov.uk>
2023-02-13 12:06:51 +0000

    black

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour="purple")
+  geom_point(colour="black"
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
