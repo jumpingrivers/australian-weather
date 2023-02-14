e21e2d745f32a0e53da7a8e7d3191c1283cc7e3f
jmccallion96 <jmccallion96@outlook.com>
2023-02-14 09:28:55 +0000

    Add wind direction

--- R/create-plot.R
+++ R/create-plot.R
@@ -16,7 +16,8 @@ scatter_plot =
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point(colour = "yellow") + 
-  theme_minimal()
+  theme_minimal() + 
+  facet_wrap(~WindDir9am)
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
