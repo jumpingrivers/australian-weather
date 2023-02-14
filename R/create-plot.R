9d72aabe095538d3088e55839da1ef304981ef63
jmccallion96 <jmccallion96@outlook.com>
2023-02-14 10:52:57 +0000

    create timeseries

--- R/create-plot.R
+++ R/create-plot.R
@@ -21,3 +21,16 @@ scatter_plot =
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
+
+###### Add time series plot
+
+avg_rainfall = df %>%
+  group_by(Location) %>%
+  summarise(avg_rainfall = mean(Rainfall, na.rm = TRUE)) %>%
+  slice(1:20)
+
+avg_rainfall_plot = ggplot(avg_rainfall, aes(x = avg_rainfall, y = Location)) +
+  geom_col()
+
+#Save the plot in graphics/
+ggsave(filename = "graphics/average-rainfall-location.png", plot = avg_rainfall_plot)
