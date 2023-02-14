97a270713391a2311105f52b90222314ef085a41
markfergusondfc <mark.ferguson@communities-ni.gov.uk>
2023-02-14 11:45:15 +0000

    chnage

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
   df %>%
   filter(Location == "Brisbane") %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) +
-  geom_point(colour = "yellow") +
+  geom_point(colour = "red") +
   labs(title = "Humidity and Temperatur for Brisbane") +
   theme_minimal()
 
