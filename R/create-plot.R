099e7bafd786caa85162fd91df7a28d9e834c4cf
jmccallion96 <jmccallion96@outlook.com>
2023-02-14 09:17:54 +0000

    Changed colour to yellow and city to Sydney

--- R/create-plot.R
+++ R/create-plot.R
@@ -12,10 +12,11 @@ df = read_csv("data/weatherAUS.csv")
 city = "Sydney"
 
 scatter_plot = 
-df %>%
+  df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour = "cyan")
+  geom_point(colour = "yellow") + 
+  theme_minimal()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
