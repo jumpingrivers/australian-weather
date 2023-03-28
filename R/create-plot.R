624c99bc039eec82c0acea5b99db13344e322ae2
ptg-gitdemo <parisa+gitdemo@jumpingrivers.com>
2023-03-28 10:07:40 +0100

    Add caption to plot

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,8 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point()
+  geom_point() +
+  labs(caption = "Copyright by the Australian Commonwealth Bureau of Meteorology")
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
