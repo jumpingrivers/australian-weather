3686f7a070e78163796260123672e41e48a6c69a
MassPing <mj7aylor86@gmail.com>
2023-03-30 12:01:50 +0100

    add caption

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,9 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(color="red")
+  geom_point(color="red")+
+  labs(caption = "Hello Sydney") +
+  theme_minimal()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
