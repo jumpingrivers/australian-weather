3047d690c54578f88a0d183ed1e660c9f0c67ef1
osheen-2048 <osheen+teachgit@jumpingrivers.com>
2025-06-09 10:38:35 +0100

    Changing city to Brisbane and adding barchart

--- R/create-plot.R
+++ R/create-plot.R
@@ -9,16 +9,24 @@ df = read_csv("data/weatherAUS.csv")
 # Create a scatter plot of Humidity9am and Temp9am
 # a location
 
-city = "Brisbae"
+city = "Brisbane"
 
 scatter_plot = 
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point( colour = "blue") +
-  ggtitle("Sydney") +
+  ggtitle("Brisbane") +
   theme_classic() + 
   facet_wrap(~WindDir9am)
+
+bar_chart =
+  df %>%
+  group_by(Location) %>%
+  summarise(average_rainfall = mean(Rainfall, na.rm = TRUE)) %>%
+  ggplot(aes(y = Location, x = average_rainfall)) +
+  geom_col()
+ggsave(filename = "graphics/barchart-avg-rainfall.png", plot = bar_chart)
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
