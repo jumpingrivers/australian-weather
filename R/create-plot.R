ee55cc5aaea499ad0674e0f013ba2359dbf6a5ad
trsketch <theo+sketch@jumpingrivers.com>
2025-06-18 11:19:05 +0100

    adding another hi

--- R/create-plot.R
+++ R/create-plot.R
@@ -16,5 +16,5 @@ df %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   geom_point()
 
-# Save the plot in graphics/
+# Save the plot in graphics/ hi
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
